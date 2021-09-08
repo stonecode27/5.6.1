2b04adba5c55476139823cd1b11824cbe10f3c1e
stonecode27 <20cestlavie1703@gmail.com>
2021-09-08 20:59:15 +0300

    Стабильная версия

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# Написать игру "Крестики-нолики"
-# Размер поля предполагается равным 3x3.
-#
-# Пример печати поля в консоль:
-#   0 1 2
-# 0 x x o
-# 1 o x -
-# 2 - o -
 import time  # Библиотека time для выставления задержек
+
 print("Добро пожаловать в игру 'Крестики-нолики'")
 print("created by stonecode27")
 
@@ -16,7 +9,6 @@ main_menu = """
 Главное меню
 Отправьте "старт", чтобы начать игру
 Отправьте "правила", чтобы узнать правила игры
-Отправьте "настройки", чтобы изменить настройки
 Отправьте "выход", чтобы выйти из программы
 
 """
@@ -34,55 +26,116 @@ rules = """
 а "y" номер строки.
 
         """
-# Варианты значений клеток
-var_tuple = ("#", "x", "o")
-# Переменные s1-s9 - "клетки" игрового поля
-s1, s2, s3, s4, s5, s6, s7, s8, s9 = var_tuple[0], var_tuple[0], var_tuple[0], \
-                                     var_tuple[0], var_tuple[0], var_tuple[0],\
-                                     var_tuple[0], var_tuple[0], var_tuple[0]
-# address_list -список адресов клеток
-address_list = ["00","10","20","01","11", "21", "02", "12", "22"]
-# var_list - cписок переменных
-var_list = [s1, s2, s3, s4, s5, s6, s7, s8, s9]
+# Переменная var_dict - словарь с адресами клеток игрового поля и их значениями
+var_dict = {"00": "#", "10": "#", "20": "#",
+            "01": "#", "11": "#", "21": "#",
+            "02": "#", "12": "#", "22": "#"
+            }
+# Переменная playing_field - форматированная строка игрового поля
+playing_field = f"""  
+              0 1 2
+            0 {var_dict["00"]} {var_dict["10"]} {var_dict["20"]}
+            1 {var_dict["01"]} {var_dict["11"]} {var_dict["21"]}
+            2 {var_dict["02"]} {var_dict["12"]} {var_dict["22"]}
+
+                        """
 # Переменная turns - текст-приглашение коммандной строки сделать ход в игре
 turns = "Отправьте адрес поля \n >"
 # Переменная address_error - текст ошибки неправильного адреса клетки
 address_error = "Поля с таким адресом не существует!"
+# Переменная round_count cчитает ходы, принимает стартовое значение 1
+round_count = 1
+# Список значений словаря var_dict
+list_of_values = list(var_dict.values())
+# Переменная finish - индикатор конца игры
+finish = None
+
 
 # Цикл, вызывающий главное меню
 while True:
-    # Переменная - форматированная строка игрового поля
-    playing_field = f"""  
-              0 1 2
-            0 {s1} {s2} {s3}
-            1 {s4} {s5} {s6}
-            2 {s7} {s8} {s9}
-
-                        """
     print(main_menu)
     navigation = input(menu)
     if navigation == "правила":  # если ввели "правила"
         print(rules)   # Вывести правила
         time.sleep(10) # Задержка на показ правил и возвращение в главное меню
     elif navigation == "старт":  # если ввели "старт"
-        while True:
-            if s1 == var_tuple[0] and s2 == var_tuple[0] and s3 == var_tuple[0]\
-                    and s4 == var_tuple[0] and s5 == var_tuple[0] and s6 == var_tuple[0]\
-                    and s7 == var_tuple[0] and s8 == var_tuple[0] and s9 == var_tuple[0]: # если начало игры и все клетки #
-                round_count = 1  # переменная round_count cчитает ходы, принимает стартовое значение
-                print("Начало игры.")
-                print(f"Ход {round_count}. Ходят 'x'")  # вывод сообщения раунда
-                print(playing_field)  # вывод стартового поля
-            address = input(turns)
-            if address in address_list:
-                round_count += 1
-                x_or_o = "o" if round_count % 2 == 0 else "x"  # переменная x_or_o, принимающая значение крестика или нолика
-                # в зависимости от хода
-                round_m = f"Раунд {round_count}. Ходят '{x_or_o}'."  # сообщение раунда
-                print(round_m)  # вывод сообщения раунда
-                print(playing_field)  # вывод игрового поля
-            else:
+        print("Начало игры.")
+        print(f"Ход {round_count}. Ходят 'х'")  # вывод сообщения раунда
+        print(playing_field)  # вывод стартового поля
+        x_or_o = "x"  # переменная, обозначающая крестик или нолик в зависисоти от очередности хода. В начале "x"
+        while True:  # Цикл While с постусловием на проверку результата
+            address = input(turns)  # ввести адресс ячейки
+            if address in var_dict:  # проверка на корректность введенного адреса
+                for i in var_dict:  # итерация по словарю var_dict
+                    if i == address:  # Если адресс клетки найден
+                        if var_dict[i] == "#":  # Если клетка не занята
+                            var_dict[i] = x_or_o  # значение по ключу словаря приняло значение переменной x_or_o
+                            round_count += 1  # новый раунд, поскольку "поставили" крестик или нолик
+                            x_or_o = "o" if round_count % 2 == 0 else "x"  # если раунд четный, то очередь 'o', иначе 'x'
+                            print(f"Ход {round_count}. Ходят '{x_or_o}'")
+                            print(f"""  
+              0 1 2
+            0 {var_dict["00"]} {var_dict["10"]} {var_dict["20"]}
+            1 {var_dict["01"]} {var_dict["11"]} {var_dict["21"]}
+            2 {var_dict["02"]} {var_dict["12"]} {var_dict["22"]}
+
+                                            """)
+                            break
+                        else:  # если клетка уже занята
+                            print("Клетка уже занята!")
+            else:  # если адрес не существует, вывести ошибку
                 print(address_error)
+            for j in list(var_dict.values()):  # цикл проверки условий окончания игры,
+                if list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[1] == j \
+                        and list(var_dict.values())[2] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[0]}'.")
+                    break
+                elif list(var_dict.values())[3] == j and j != "#" and list(var_dict.values())[4] == j \
+                        and list(var_dict.values())[5] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[3]}'.")
+                    break
+                elif list(var_dict.values())[6] == j and j != "#" and list(var_dict.values())[7] == j \
+                        and list(var_dict.values())[8] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
+                    break
+                elif list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[3] == j \
+                        and list(var_dict.values())[6] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[6]}'.")
+                    break
+                elif list(var_dict.values())[1] == j and j != "#" and list(var_dict.values())[4] == j \
+                        and list(var_dict.values())[7] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[7]}'.")
+                    break
+                elif list(var_dict.values())[2] == j and j != "#" and list(var_dict.values())[5] == j \
+                        and list(var_dict.values())[8] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
+                    break
+                elif list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[4] == j \
+                        and list(var_dict.values())[8] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
+                    break
+                elif list(var_dict.values())[2] == j and j != "#" and list(var_dict.values())[4] == j \
+                        and list(var_dict.values())[6] == j:
+                    finish = 1
+                    print(f"Конец игры. Победили '{list(var_dict.values())[6]}'.")
+                    break
+                elif not ("#" in list(var_dict.values())):
+                    finish = 1
+                    print(f"Конец игры. Ничья.")
+                    break
+
+            if not (finish is None):  # Одно из условий окончания игры сработало и изменило переменную finish.
+                # Цикл игры завершает работу
+                time.sleep(3)  # задержка на возврат в главное меню
+                break
+
 
     elif navigation == "выход":
         print("Выход из игры...")
--- tttonly.py
+++ tttonly.py
@@ -1,8 +1,9 @@
-s1, s2, s3, s4, s5, s6, s7, s8, s9 = "#", "#", "#", "#", "#", "#", "#", "#", "#"
-var_dict = {"00": s1, "10": s2, "20": s3,
-            "01": s4, "11": s5, "21": s6,
-            "02": s7, "12": s8, "22": s9
+# Переменная var_dict - словарь с адресами клеток игрового поля и их значениями
+var_dict = {"00": "#", "10": "#", "20": "#",
+            "01": "#", "11": "#", "21": "#",
+            "02": "#", "12": "#", "22": "#"
             }
+# Переменная playing_field - форматированная строка игрового поля
 playing_field = f"""  
               0 1 2
             0 {var_dict["00"]} {var_dict["10"]} {var_dict["20"]}
@@ -14,7 +15,7 @@ playing_field = f"""
 turns = "Отправьте адрес поля \n >"
 # Переменная address_error - текст ошибки неправильного адреса клетки
 address_error = "Поля с таким адресом не существует!"
-# Переменная round_count cчитает ходы, принимает стартовое значение
+# Переменная round_count cчитает ходы, принимает стартовое значение 1
 round_count = 1
 # Список значений словаря var_dict
 list_of_values = list(var_dict.values())
@@ -27,13 +28,13 @@ print(playing_field)  # вывод стартового поля
 x_or_o = "x"  # переменная, обозначающая крестик или нолик в зависисоти от очередности хода. В начале "x"
 while True:  # Цикл While с постусловием на проверку результата
     address = input(turns)  # ввести адресс ячейки
-    if address in var_dict: # проверка на корректность введенного адреса
+    if address in var_dict:  # проверка на корректность введенного адреса
         for i in var_dict:  # итерация по словарю var_dict
             if i == address:  # Если адресс клетки найден
                 if var_dict[i] == "#":  # Если клетка не занята
                     var_dict[i] = x_or_o  # значение по ключу словаря приняло значение переменной x_or_o
                     round_count += 1  # новый раунд, поскольку "поставили" крестик или нолик
-                    x_or_o = "o" if round_count % 2 == 0 else "x"  # если раунд четный, то очередь ноликов, иначе крестиков
+                    x_or_o = "o" if round_count % 2 == 0 else "x"  # если раунд четный, то очередь 'o', иначе 'x'
                     print(f"Ход {round_count}. Ходят '{x_or_o}'")
                     print(f"""  
                           0 1 2
@@ -51,46 +52,46 @@ while True:  # Цикл While с постусловием на проверку 
         if list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[1] == j\
                 and list(var_dict.values())[2] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[0]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[0]}'.")
             break
         elif list(var_dict.values())[3] == j and j != "#" and list(var_dict.values())[4] == j\
                 and list(var_dict.values())[5] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[3]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[3]}'.")
             break
         elif list(var_dict.values())[6] == j and j != "#" and list(var_dict.values())[7] == j\
                 and list(var_dict.values())[8] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
             break
         elif list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[3] == j\
                 and list(var_dict.values())[6] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[6]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[6]}'.")
             break
         elif list(var_dict.values())[1] == j and j != "#" and list(var_dict.values())[4] == j\
                 and list(var_dict.values())[7] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[7]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[7]}'.")
             break
         elif list(var_dict.values())[2] == j and j != "#" and list(var_dict.values())[5] == j\
                 and list(var_dict.values())[8] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
             break
         elif list(var_dict.values())[0] == j and j != "#" and list(var_dict.values())[4] == j\
                 and list(var_dict.values())[8] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[8]}'.")
             break
         elif list(var_dict.values())[2] == j and j != "#" and list(var_dict.values())[4] == j\
                 and list(var_dict.values())[6] == j:
             finish = 1
-            print(f"Конец игры. Победили '{list(var_dict.values())[6]}'")
+            print(f"Конец игры. Победили '{list(var_dict.values())[6]}'.")
             break
         elif not ("#" in list(var_dict.values())):
             finish = 1
-            print(f"Конец игры. Ничья")
+            print(f"Конец игры. Ничья.")
             break
 
     if not (finish is None):  # Одно из условий окончания игры сработало и изменило переменную finish.
